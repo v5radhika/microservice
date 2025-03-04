7f8476023dedc8fe7bbf50a6678cdd715ec4f348
RADHIKA VEMULA <85382039+v5radhika@users.noreply.github.com>
2025-03-04 16:57:15 +0530

    Implementation of microservice
    Implementation of microservice

---
+++ src/main/java/com/project/sample/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.project.sample.repository;
+
+import com.project.sample.model.Employee;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends MongoRepository<Employee, Long> {
+
+}
